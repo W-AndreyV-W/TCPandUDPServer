ad26f06ba9387369b6f9c9a8cbce94ce71cedfd6
Andrey V <117563643+W-AndreyV-W@users.noreply.github.com>
2025-11-28 22:15:53 +0500

    first commitДобавьте файлы проекта.

---
+++ TCPandUDPServer.cpp
@@ -0,0 +1,15 @@
+﻿// TCPandUDPServer.cpp: определяет точку входа для приложения.
+//
+
+#include "TCPandUDPServer.h"
+#include "MultiplexingSocket.h"
+#include <iostream>
+
+int main()
+{
+	MultiplexingSocket MultiplexingSocket;
+
+	std::cout << "3 2 1 0" << std::endl;
+
+	return 0;
+}
